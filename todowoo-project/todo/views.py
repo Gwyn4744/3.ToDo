2e2d8e04d151183154bdddd17f08384474f1c122
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-05 22:35:34 +0200

    created the signupuser page v0.0.5

--- todowoo-project/todo/views.py
+++ todowoo-project/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def signupuser(request):
+    contex = {
+        'form': UserCreationForm
+    }
+    return render(request, 'todo/signupuser.html', contex)
