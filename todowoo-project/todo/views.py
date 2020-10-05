ea589be35b35cfb1d6953dc271cfe109e6b65e34
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-05 22:51:00 +0200

    Added the create user features v0.0.6

--- todowoo-project/todo/views.py
+++ todowoo-project/todo/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupuser(request):
-    contex = {
-        'form': UserCreationForm
-    }
-    return render(request, 'todo/signupuser.html', contex)
+    if request.method == 'GET':
+        contex = {
+            'form': UserCreationForm,
+        }
+        return render(request, 'todo/signupuser.html', contex)
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # Create a new user
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+            # Passwords dodn't match
+            contex = {
+                'form': UserCreationForm,
+                'error': 'Podane hasła są różne!',
+            }
+            return render(request, 'todo/signupuser.html', contex)
