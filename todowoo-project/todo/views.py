cb50ef704d362c59fd95112e901b5513795fa50a
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-06 08:55:47 +0200

    Created the list todos page (filternig data) v0.1.3

--- todowoo-project/todo/views.py
+++ todowoo-project/todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
 
 # Create your views here.
 
@@ -37,7 +38,11 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html', contex)
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    contex = {
+        'todos': todos,
+    }
+    return render(request, 'todo/currenttodos.html', contex)
 
 def loginuser(request):
     if request.method == 'GET':
